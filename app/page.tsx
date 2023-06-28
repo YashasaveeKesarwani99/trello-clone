670c9dc6eeda8dea255f39063bb395dff2034b67
YashasaveeKesarwani99 <yashasvikesarwani1@gmail.com>
2023-06-28 22:16:31 +0530

    some progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-   <main>
-    Hello Trello 2.0
+    <main>
+      {/* header */}
+      <Header />
     </main>
-  )
+  );
 }
